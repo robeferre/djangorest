41f5c31306f1ba03ca7ab463fdd3627d12b78b7f
Roberto Ferreira Junior <roberto.ferreira@tvglobo.com.br>
2016-09-28 16:50:16 -0300

    Initial project

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url, include
 """tutorial URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,4 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('snippets.urls')),
 ]
